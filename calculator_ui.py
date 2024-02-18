bc8120f2bc3b669c1e90ed32eaca1a45d1190b98
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-02-18 15:40:14 +0700

    add set_display and get_display

--- calculator_ui.py
+++ calculator_ui.py
@@ -12,17 +12,16 @@ class CalculatorUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Calculator')
-        self.text = tk.StringVar()
+        self.display_text = tk.StringVar()
         self.default_font = font.nametofont('TkDefaultFont')
         self.default_font.configure(family='Arial TUR', size=16)
         self.create_display()
         self.create_buttons()
 
     def create_display(self):
-        display = tk.Entry(self, justify='right', textvariable=self.text, state='disabled')
+        display = tk.Entry(self, justify='right', textvariable=self.display_text, state='disabled')
         display.config(bg='Black', fg='Yellow', font=('Times',30))
         display.pack()
-        
 
     def create_buttons(self):
         top_row_keys = [' ( ',' ) ','CLR','DEL']
@@ -34,18 +33,12 @@ class CalculatorUI(tk.Tk):
         top_row_buttons.pack(anchor=tk.N, **self.PACKOPTION)
         keypad.pack(side=tk.LEFT, **self.PACKOPTION)
         operators.pack(side=tk.RIGHT, **self.PACKOPTION)
-        
 
-    def init_components(self):
-        keypad = Keypad(self, keynames=list('789456123 0.'), columns=3)
-        operators = Keypad(self, keynames=list('*/+-^='))
-        keypad.bind('<Button>', self.keypress_handler)
-        operators.bind('<Button>', self.keypress_handler)
-        output = tk.Label(self, bg='Black', fg='Yellow', anchor=tk.E, 
-                          textvariable=self.text, font=('Times',30))
-        output.pack(side=tk.TOP, **self.PACKOPTION)
-        keypad.pack(side=tk.LEFT, **self.PACKOPTION)
-        operators.pack(side=tk.RIGHT, **self.PACKOPTION)
+    def set_display_text(self, text):
+        self.display_text.set(text)
+
+    def get_display_text(self):
+        return self.display_text.get()
 
     def run(self):
         """Start the app"""
