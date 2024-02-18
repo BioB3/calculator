ba12f406c25cde7f802c7815633b8da6c82f3690
Phasin Sae-ngow <bird.biob3@gmail.com>
2024-02-18 16:57:38 +0700

    add calculate

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 """Controller for Calculator app"""
+import tkinter as tk
 from calculator_ui import CalculatorUI
 from model import Model
 
@@ -10,14 +11,27 @@ class Controller:
         self.bind_command()
 
     def update_display(self, event):
+        """Update display's text"""
         pressed_text = event.widget['text']
         self.view.set_display_text(self.view.get_display_text()+pressed_text)
 
+    def calculate(self, *args):
+        """Calculate the expression on the display"""
+        result = self.model.evaluate_expression(self.view.get_display_text())
+        self.view.set_display_text(result)
+
     def bind_command(self):
         """bind commands to buttons based on thier text"""
-        for frame in self.view.winfo_children():
-            for component in frame.winfo_children():
-                component.bind('<Button>', self.update_display)
+        for component in self.view.winfo_children():
+            if isinstance(component,tk.Frame):
+                for button in component.winfo_children():
+                    if button['text'] == 'CLR':
+                        button.bind('<Button>', lambda event, x='' :
+                            self.view.set_display_text(x))
+                    elif button['text'] == '=':
+                        button.bind('<Button>', self.calculate)
+                    elif button['text'] not in ['=','CLR','DEL']:
+                        button.bind('<Button>', self.update_display)
 
     def run(self):
         self.view.run()
